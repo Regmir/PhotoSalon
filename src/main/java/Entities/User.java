e2446df96632d71983b97c5f76b7a8c11b00b884
Regmir <arteom.lap@yandex.ru>
2019-05-02 16:30:30 +0300

    Registration/login

--- src/main/java/Entities/User.java
+++ src/main/java/Entities/User.java
@@ -1,9 +1,45 @@
 package Entities;
 
+import dataBaseManagement.model.ObjectFromDB;
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 
-public class User {
+public class User implements Serializable {
+
     List<Order> orders;
     HashMap<String, String> params;//хранение параметров личного кабинета и т д
+
+    public User(String name, String password, String email, String admin){
+        this.params.put("name",name);
+        this.params.put("password",password);
+        this.params.put("email",email);
+        this.params.put("admin",admin);
+    }
+
+    public ObjectFromDB prepareObjectFromDB(){
+        ObjectFromDB objToPersist = new ObjectFromDB();
+        objToPersist.setName(params.get("name"));
+        objToPersist.setType("user");
+        byte[] parameters =  SerializationUtils.serialize(this);
+        objToPersist.setParameters(parameters);
+        return  objToPersist;
+    }
+
+    public boolean authentificate(String password){
+        return params.get("password").equals(password);
+    }
+
+    public boolean isadmin(){
+        return params.get("admin").equals("yes");
+    }
+
+    public static User parseUser(ObjectFromDB objectFromDB){
+        User user = null;
+        if (objectFromDB.getType().equals("user"))
+            user = (User)SerializationUtils.deserialize(objectFromDB.getParameters());
+        return user;
+    }
 }
--- src/main/java/controllers/ObjectsFromDBController.java
+++ src/main/java/controllers/ObjectsFromDBController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import Entities.User;
 import dataBaseManagement.model.ObjectFromDB;
 import dataBaseManagement.service.ObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,45 @@ public class ObjectsFromDBController {
     public String addObj(@ModelAttribute("obj") ObjectFromDB obj){
             this.objectService.addObject(obj);
         return "redirect:/objects";
+    }
+
+    @RequestMapping(value = "/user/add", method = RequestMethod.POST)
+    public String addUser(@RequestParam ("name") String name,
+                          @RequestParam ("pass") String pass,
+                          @RequestParam ("secret") String secret,
+                          @RequestParam ("email") String email, Model model){
+        if (secret.equals("admin"))
+            secret="yes";
+        else
+            secret="no";
+        User user = new User(name,pass,email,secret);
+        ObjectFromDB obj = user.prepareObjectFromDB();
+        this.objectService.addObject(obj);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST)
+    public String login(@RequestParam ("name") String name,
+                          @RequestParam ("pass") String pass, Model model){
+        ObjectFromDB obj = this.objectService.getObject(name,"user");
+        if (obj!=null){
+            User user = User.parseUser(obj);
+            if (user.authentificate(pass)) {
+                if (user.isadmin()) return  "adminMainPage";
+                else return "userMainPage";
+            }
+        }
+        return "authentificationFailed";
+    }
+
+    @RequestMapping("/login")
+    public String login( Model model){
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register( Model model){
+        return "register";
     }
 
     @RequestMapping(value = "/perceptron/add", method = RequestMethod.POST)
